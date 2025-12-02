3369c81988a3ec1d26b87791998c9cbfac06427e
Lars Nyström <nystrom.lasse@gmail.com>
2025-12-02 01:10:14 +0100

    Solution to day 1

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("day01/input.txt");
+var inputRegex = new Regex(@"^(?<direction>[LR])(?<steps>\d+)$");
+
+var dial = new Dial(100, 50);
+foreach (var line in lines)
+{
+    var match = inputRegex.Match(line);
+    var direction = match.Groups["direction"].Value;
+    var steps = int.Parse(match.Groups["steps"].Value);
+
+    if (direction == "L")
+    {
+        dial.Turn(-steps);
+    }
+    else if (direction == "R")
+    {
+        dial.Turn(steps);
+    }
+}
+Console.WriteLine($"The dial returned to position 0 a total of {dial.GetZeroPositionStopCount()} times.");
+Console.WriteLine($"The dial visited position 0 a total of {dial.GetZeroPositionVisitCount()} times.");
+
+public class Dial
+{
+    private int position;
+    private readonly int size;
+    private int countZeroPositionStops = 0;
+    private int countZeroPositionVisits = 0;
+
+    public Dial(int size, int startPosition)
+    {
+        this.size = size;
+        this.position = startPosition;
+    }
+
+    public void Turn(int steps)
+    {
+        int previousPosition = position;
+        int entireRotations = steps / size;
+        countZeroPositionVisits += Math.Abs(entireRotations);
+        position = (position + steps) % size;
+        if (position < 0)
+        {
+            position += size;
+        }
+        if (Math.Sign(steps) < 0 && position > previousPosition && previousPosition != 0)
+        {
+            countZeroPositionVisits++;
+        }
+        else if (Math.Sign(steps) > 0 && position < previousPosition && position != 0)
+        {
+            countZeroPositionVisits++;
+        }
+        if (position == 0)
+        {
+            countZeroPositionStops++;
+            countZeroPositionVisits++;
+        }
+    }
+
+    public int GetPosition()
+    {
+        return position;
+    }
+
+    public int GetZeroPositionStopCount()
+    {
+        return countZeroPositionStops;
+    }
+
+    public int GetZeroPositionVisitCount()
+    {
+        return countZeroPositionVisits;
+    }
+}
---
+++ tests/day01tests/UnitTests.cs
@@ -0,0 +1,56 @@
+﻿namespace day01tests;
+
+public class UnitTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var dial = new Dial(100, 50);
+        dial.Turn(-68);
+        Assert.Equal(82, dial.GetPosition());
+        Assert.Equal(0, dial.GetZeroPositionStopCount());
+        Assert.Equal(1, dial.GetZeroPositionVisitCount());
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var dial = new Dial(100, 50);
+        dial.Turn(-68);
+        dial.Turn(-30);
+        dial.Turn(48);
+        Assert.Equal(0, dial.GetPosition());
+        Assert.Equal(1, dial.GetZeroPositionStopCount());
+        Assert.Equal(2, dial.GetZeroPositionVisitCount());
+        dial.Turn(-5);
+        dial.Turn(60);
+        Assert.Equal(55, dial.GetPosition());
+        Assert.Equal(1, dial.GetZeroPositionStopCount());
+        Assert.Equal(3, dial.GetZeroPositionVisitCount());
+        dial.Turn(-55);
+        Assert.Equal(0, dial.GetPosition());
+        Assert.Equal(2, dial.GetZeroPositionStopCount());
+        Assert.Equal(4, dial.GetZeroPositionVisitCount());
+        dial.Turn(-1);
+        dial.Turn(-99);
+        dial.Turn(14);
+        dial.Turn(-82);
+        Assert.Equal(32, dial.GetPosition());
+        Assert.Equal(3, dial.GetZeroPositionStopCount());
+        Assert.Equal(6, dial.GetZeroPositionVisitCount());
+        dial.Turn(100);
+        Assert.Equal(32, dial.GetPosition());
+        Assert.Equal(3, dial.GetZeroPositionStopCount());
+        Assert.Equal(7, dial.GetZeroPositionVisitCount());
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var dial = new Dial(100, 50);
+        dial.Turn(-399);
+        Assert.Equal(51, dial.GetPosition());
+        Assert.Equal(0, dial.GetZeroPositionStopCount());
+        Assert.Equal(4, dial.GetZeroPositionVisitCount());
+    }
+}
