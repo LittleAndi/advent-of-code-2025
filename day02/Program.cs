1c11f127d69aee1b29619fcf5d0c37d1ebdbcbf5
Lars Nyström <nystrom.lasse@gmail.com>
2025-12-25 16:07:34 +0100

    Solution for day 2

--- day02/Program.cs
+++ day02/Program.cs
@@ -1,2 +1,107 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("day02/input.txt");
+
+var db = new GiftShopDatabase(lines[0]);
+Console.WriteLine($"Sum of all invalid product IDs: {db.GetSumOfAllInvalidProductIds()}");
+
+Console.WriteLine($"Sum of all extended invalid product IDs: {db.GetSumOfAllExtendedInvalidProductIds()}");
+
+public class GiftShopDatabase
+{
+    private readonly Dictionary<string, (long Start, long End)> productIdRanges = [];
+    public GiftShopDatabase(string init)
+    {
+        var initParser = new Regex(@"((\d+)-(\d+))");
+        var matches = initParser.Matches(init);
+        foreach (Match match in matches)
+        {
+            var productIdRangeKey = match.Groups[0].Value;
+            var rangeStart = long.Parse(match.Groups[2].Value);
+            var rangeEnd = long.Parse(match.Groups[3].Value);
+            productIdRanges[productIdRangeKey] = (rangeStart, rangeEnd);
+        }
+    }
+
+    public long[] GetInvalidProductIds(string rangeKey)
+    {
+        var start = productIdRanges[rangeKey].Start;
+        var end = productIdRanges[rangeKey].End;
+
+        var invalidIds = new List<long>();
+
+        for (long id = start; id <= end; id++)
+        {
+            if (id.ToString().Length % 2 != 0)
+            {
+                continue;
+            }
+            var idStr = id.ToString();
+            var leftHalf = idStr[..(idStr.Length / 2)];
+            var rightHalf = idStr[(idStr.Length / 2)..];
+            if (leftHalf == rightHalf)
+            {
+                invalidIds.Add(id);
+            }
+        }
+        return [.. invalidIds];
+    }
+
+    public long[] GetExtendedInvalidProductIds(string rangeKey)
+    {
+        var start = productIdRanges[rangeKey].Start;
+        var end = productIdRanges[rangeKey].End;
+        var regex = new Regex(@"^(\d+)\1+$");
+
+        var invalidIds = new List<long>();
+
+        for (long id = start; id <= end; id++)
+        {
+            var idStr = id.ToString();
+
+            bool isInvalid = false;
+            if (regex.IsMatch(idStr))
+            {
+                isInvalid = true;
+            }
+            else
+            {
+                var leftHalf = idStr[..(idStr.Length / 2)];
+                var rightHalf = idStr[(idStr.Length / 2)..];
+                if (leftHalf == rightHalf)
+                {
+                    isInvalid = true;
+                }
+            }
+
+            if (isInvalid)
+            {
+                invalidIds.Add(id);
+            }
+        }
+        return [.. invalidIds];
+    }
+
+    public long GetSumOfAllInvalidProductIds()
+    {
+        var allInvalidIds = new List<long>();
+        foreach (var rangeKey in productIdRanges.Keys)
+        {
+            var invalidIds = GetInvalidProductIds(rangeKey);
+            allInvalidIds.AddRange(invalidIds);
+        }
+        return allInvalidIds.Sum(id => (long)id);
+    }
+
+    public long GetSumOfAllExtendedInvalidProductIds()
+    {
+        var allInvalidIds = new List<long>();
+        foreach (var rangeKey in productIdRanges.Keys)
+        {
+            var invalidIds = GetExtendedInvalidProductIds(rangeKey);
+            allInvalidIds.AddRange(invalidIds);
+        }
+        return allInvalidIds.Sum(id => (long)id);
+    }
+}
---
+++ tests/day02tests/UnitTests.cs
@@ -0,0 +1,44 @@
+﻿namespace day02tests;
+
+public class UnitTests
+{
+    [Theory]
+    [InlineData("11-22", 2, new long[] { 11, 22 })]
+    [InlineData("95-115", 1, new long[] { 99 })]
+    public void Test1(string rangeKey, int expectedInvalidCount, long[] expectedInvalidIds)
+    {
+        var db = new GiftShopDatabase("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124");
+        var invalidIds = db.GetInvalidProductIds(rangeKey);
+        Assert.Equal(expectedInvalidCount, invalidIds.Length);
+        Assert.Equal(expectedInvalidIds, invalidIds);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var db = new GiftShopDatabase("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124");
+        var sumOfAllInvalidProductIds = db.GetSumOfAllInvalidProductIds();
+        Assert.Equal(1227775554, sumOfAllInvalidProductIds);
+    }
+
+    [Theory]
+    [InlineData("11-22", 2, new long[] { 11, 22 })]
+    [InlineData("95-115", 2, new long[] { 99, 111 })]
+    public void Test3(string rangeKey, int expectedInvalidCount, long[] expectedInvalidIds)
+    {
+        var db = new GiftShopDatabase("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124");
+        var invalidIds = db.GetExtendedInvalidProductIds(rangeKey);
+        Assert.Equal(expectedInvalidCount, invalidIds.Length);
+        Assert.Equal(expectedInvalidIds, invalidIds);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var db = new GiftShopDatabase("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124");
+        var sumOfAllExtendedInvalidProductIds = db.GetSumOfAllExtendedInvalidProductIds();
+        Assert.Equal(4174379265, sumOfAllExtendedInvalidProductIds);
+    }
+
+}
+
